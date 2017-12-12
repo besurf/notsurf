72809bec6915c57574dbb6938b440873c8db2bb4
Shekhar Tyagi <shekhar.rdj@yahoo.com>
2017-12-12 11:11:24 +0530

    Update notsurf.js

--- notsurf.js
+++ notsurf.js
@@ -1,1 +1,15 @@
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/pwabuilder-sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 //script goes here
+var myHeader = document.getElementsByClassName('header')
+var offSet = 50;
